8fd104608034972442c625cf641e2db4c9937571
camilo <chc201627@gmail.com>
2018-11-21 20:15:06 -0500

    Agregando TextFile y Button

--- index.php
+++ index.php
@@ -10,19 +10,21 @@ $active="active";
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title><?php echo $title;?></title>
 
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/style.css">
+
   </head>
   <body>
-  <hr>
-    <div class="row-fluid">
-    <div class="container">
+
+<!--    <div class="row-fluid">-->
+<!--    <div class="container">-->
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-9">
-			<div id="carousel-example-captions" class="carousel slide" data-ride="carousel"> 
+			<div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
 				<?php
                     $sql = "select * from slider where estado=1 order by orden";
                     $sql_slider = sqlsrv_query($con,$sql);
@@ -36,7 +38,7 @@ $active="active";
 
 			?>
 					<ol class="carousel-indicators">
-						<?php 
+						<?php
 						for ($i=0; $i<$nums_slides; $i++){
 							$active="active";
 							?>
@@ -45,44 +47,43 @@ $active="active";
 							$active="";
 						}
 						?>
-						
-					</ol> 
-				<div class="carousel-inner" role="listbox"> 
+
+					</ol>
+				<div class="carousel-inner" role="listbox">
 				<?php
 					$active="active";
 					while ($rw_slider=sqlsrv_fetch_array($sql_slider)){
 				?>
-						<div class="item <?php echo $active;?>"> 
-							<img data-src="holder.js/900x500/auto/#777:#777" alt="900x500" src="img/slider/<?php echo $rw_slider['url_image'];?>" data-holder-rendered="true"> 
-							<div class="carousel-caption"> 
+						<div class="item <?php echo $active;?>">
+							<img data-src="holder.js/900x500/auto/#777:#777" alt="900x500" src="img/slider/<?php echo $rw_slider['url_image'];?>" data-holder-rendered="true">
+							<div class="carousel-caption">
 								<h3><?php echo $rw_slider['titulo'];?></h3>
-								<p><?php echo $rw_slider['descripcion'];?></p> 
+								<p><?php echo $rw_slider['descripcion'];?></p>
 								<a class='btn btn-<?php echo $rw_slider['estilo_boton'];?> text-right' href="<?php echo $rw_slider['url_boton'];?>"><?php echo $rw_slider['texto_boton'];?></a>
-							</div> 
+							</div>
 						</div>
 						<?php
 						$active="";
 					}
 				?>
-					
-					 
-					
-				</div> 
-				<a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> 
-				<a class="right carousel-control" href="#carousel-example-captions" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> 
+
+
+
+				</div>
+<!--				<a class="left carousel-control" href="#carousel-example-captions" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> -->
+<!--				<a class="right carousel-control" href="#carousel-example-captions" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> -->
 			</div>
         </div>
-		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
-			<h4 class='text-center'>Sistema de cotizaciones</h4>
-			<a href="http://obedalvarado.pw/cotizador-web/" target="_blank"><img src="http://obedalvarado.pw/img/cotizador-banner.png" class="img-responsive"></a>
-			<hr>
-			<h4 class='text-center'>Sistema de inventario</h4>
-			<a href="http://obedalvarado.pw/sistema-de-control-de-inventario/" target="_blank"><img src="http://obedalvarado.pw/img/sistema-control-inventario.png" class="img-responsive"></a>
-		</div>
-          
-    </div>
-	
-</div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-9">
+            <form action="" method="post">
+            <input type="text" class="texto form-control" name="codigo">
+            <input type="button" value="CONSULTAR PRECIO" class="btn_consultar btn btn-default btn-info form-control btn-lg" name="consultar">
+            </form>
+        </div>
+
+<!--    </div>-->
+
+<!--</div>-->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
